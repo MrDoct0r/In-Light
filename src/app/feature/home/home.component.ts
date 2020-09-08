45d10b9e10423b9afd2ee082363894e43257bc4e
LoicL66 <loic.labede@outlook.fr>
2020-09-08 17:31:15 +0200

    FEAT: get latest movies (service + init dans le component)

--- src/app/feature/home/home.component.ts
+++ src/app/feature/home/home.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ResultApi } from 'src/app/shared/models/result-api';
+import { Movie } from 'src/app/shared/models/movie';
+import { ActivatedRoute } from '@angular/router';
+import { MovieService } from 'src/app/shared/services/movie.service';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +12,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  public mostPopularMovies$: Observable<ResultApi<Movie>>;
+  public latestMovies$: Observable<ResultApi<Movie>>;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private movieService: MovieService
+  ) {
+  }
 
   ngOnInit(): void {
+    this.latestMovies$ = this.movieService.getLatest( );
   }
 
 }
--- src/app/shared/services/movie.service.ts
+++ src/app/shared/services/movie.service.ts
@@ -18,8 +18,8 @@ export class MovieService {
   ) {
   }
 
-  findByTitle(title: string, page: number): Observable<ResultApi<MovieImpl>> {
-    return this.http.get<ResultApi<MovieImpl>>( this.baseUrl + 'search/movie?query=' + title + '&page=' + page )
+  getLatest(): Observable<ResultApi<MovieImpl>> {
+    return this.http.get<ResultApi<MovieImpl>>( this.baseUrl + 'search/movie?sort_by=release_date.desc' )
       .pipe(
         map( r => {
           r.results = r.results.filter( m => m.poster_path ).map( m => MovieImpl.fromPlainObject( m ) );
@@ -32,4 +32,16 @@ export class MovieService {
   getById(id: string): Observable<Movie> {
     return this.http.get<Movie>( this.baseUrl + 'movie/' + id );
   }
+
+  findByTitle(title: string, page: number): Observable<ResultApi<MovieImpl>> {
+    return this.http.get<ResultApi<MovieImpl>>( this.baseUrl + 'search/movie?query=' + title + '&page=' + page )
+      .pipe(
+        map( r => {
+          r.results = r.results.filter( m => m.poster_path ).map( m => MovieImpl.fromPlainObject( m ) );
+          return r;
+        } ),
+        tap(console.log),
+      );
+  }
+
 }
