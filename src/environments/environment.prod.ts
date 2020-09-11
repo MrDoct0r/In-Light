ab9a7dec2450fd91f7e3dee580abecd410dcccc4
MrDoct0r <contact@alexisjeanne.fr>
2020-09-11 11:54:11 +0200

    Correction genres

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiURL: 'https://api.themoviedb.org/3/',
   apiKey: 'df2d18373c7770df1bb059dd1b08d2ba',
-  apiImageUrl: 'https://image.tmdb.org/t/p/w300/',
+  apiImageUrl: 'https://image.tmdb.org/t/p/w500/',
 };
