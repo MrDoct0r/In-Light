c4d5006e68c3a059eb4c365c2062bfed1f82280b
LoicL66 <loic.labede@outlook.fr>
2020-09-11 10:03:20 +0200

    Feat : WIP - page détails

--- src/app/feature/movies/details/details.component.ts
+++ src/app/feature/movies/details/details.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { MovieImpl, Movie } from 'src/app/shared/models/movie';
+import { MovieService } from 'src/app/shared/services/movie.service';
+import { Observable } from 'rxjs';
+import { ResultApi } from 'src/app/shared/models/result-api';
+import { Route, ActivatedRoute } from '@angular/router';
+import { switchMap, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-details',
@@ -7,9 +13,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsComponent implements OnInit {
 
-  constructor() { }
+  public movie$: Observable<MovieImpl>;
+  constructor(private movieService: MovieService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    
+    this.movie$ = this.route.params.pipe(
+      map(p => p.id),
+      switchMap(id => this.movieService.getById(id)),
+    );
   }
-
 }
